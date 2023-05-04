b0de2fadaf6880835b747ddc436e9bac99540f54
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-05-04 08:47:01 +0100

    signup form handler .js added

---
+++ public/js/sign-up.js
@@ -0,0 +1,29 @@
+const signUpHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if(username && email && password) {
+        console.log('sign-up.js')
+        console.log(username)
+        console.log(email)
+        console.log(password)
+
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        console.log(response)
+        if(response.ok) {
+            document.location.replace('/') //change later to dashboard
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signUpHandler);
