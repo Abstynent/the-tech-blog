ee01c22132b568caa14d3a5b45f096e2205fccfb
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-05-04 09:01:51 +0100

    / route created, to render all post from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/withAuth');
 
-router.get('/', (req, res) => {
-    res.render('homepage');
-    // try {
-    //     res.render('homepage');
-    // } catch (error) {
-    //     res.status(500).json(error);
-    // }
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 router.get('/login', (req, res) => {
